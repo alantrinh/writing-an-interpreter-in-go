4747ac7f952d846959e0315085dd2ba8701b3bf2
Alan <alan.l.trinh@gmail.com>
2024-01-24 12:46:11 +0100

    added single char tokens

--- token/token.go
+++ token/token.go
@@ -16,8 +16,15 @@ const (
 	INT   = "INT"   // 1234567890
 
 	// Operators
-	ASSIGN = "="
-	PLUS   = "+"
+	ASSIGN   = "="
+	PLUS     = "+"
+	MINUS    = "-"
+	BANG     = "!"
+	SLASH    = "/"
+	ASTERISK = "*"
+
+	LT = "<"
+	GT = ">"
 
 	// Delimiters
 	COMMA     = ","
